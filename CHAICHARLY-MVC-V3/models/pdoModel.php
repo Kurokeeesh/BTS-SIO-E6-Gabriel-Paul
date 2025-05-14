dbb72b831c096bdc193e7240bc63f4c84f37efb3
Gabriel PAUL <163430827+Kurokeeesh@users.noreply.github.com>
2025-05-14 12:34:39 +0200

    Update pdoModel.php

--- CHAICHARLY-MVC-V3/models/pdoModel.php
+++ CHAICHARLY-MVC-V3/models/pdoModel.php
@@ -4,7 +4,6 @@ function setDB(){
     static $pdo;
 
     if($pdo === null){
-        //$pdo = new PDO("mysql:host=chaichx45.mysql.db; dbname=chaichx45","chaichx45","Charles63200");
         $pdo = new PDO("mysql:host=localhost; dbname=bdchaicharly","root","");
         $pdo->setAttribute(PDO::ATTR_ERRMODE,
         PDO::ERRMODE_EXCEPTION);
